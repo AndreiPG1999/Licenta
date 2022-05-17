9c2b480292bac5afdd3eb8e71eb2c05f697e86bb
AndreiPG1999 <andreipopescu052@gmail.com>
2022-05-17 23:20:52 +0300

    afisare formular done, mai e butonu de pdf

--- Front/src/app/afisare-formular-pacient/afisare-formular-pacient.component.ts
+++ Front/src/app/afisare-formular-pacient/afisare-formular-pacient.component.ts
@@ -1,4 +1,9 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Formular } from '../formular';
+import { FormularService } from '../formular.service';
+import { TokenStorageService } from '../token-storage.service';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-afisare-formular-pacient',
@@ -7,9 +12,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AfisareFormularPacientComponent implements OnInit {
 
-  constructor() { }
+  currentUser !: any
+  currentFormular !: any
+  afectiuniSplitted !: any
+  alergiiSplitted !: any
+  constructor(private userService:UserService, private formularService:FormularService, private token:TokenStorageService) { }
 
   ngOnInit(): void {
+    this.currentUser = this.token.getUser()
+    this.formularService.findFormular(this.currentUser.email).subscribe({
+      next:(response: Formular) => {
+        this.currentFormular = response;
+        console.log(this.currentFormular);
+        this.afectiuniSplitted = this.currentFormular.afectiuni.split(",");
+        this.afectiuniSplitted.pop();
+        console.log(this.afectiuniSplitted);
+        this.alergiiSplitted = this.currentFormular.alergii.split(",");
+        this.alergiiSplitted.pop();
+        console.log(this.alergiiSplitted);
+      },
+      error: (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    })
   }
 
 }
