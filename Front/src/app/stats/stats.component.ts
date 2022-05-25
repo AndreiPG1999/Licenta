0d7cad1281b820829a2b232eff7af6a91473be41
AndreiPG1999 <andreipopescu052@gmail.com>
2022-05-25 23:10:46 +0300

    stats values not working

--- Front/src/app/stats/stats.component.ts
+++ Front/src/app/stats/stats.component.ts
@@ -17,40 +17,40 @@ export class StatsComponent implements OnInit {
   currentUser !: any;
   loggedInUser !: any;
   istorics !: Istoric[];
-  nrAbces !: number;
-  nrAmeloblastom !: number;
-  nrAngina !: number;
-  nrDiastema !: number;
-  nrGingivita !: number;
-  nrCarie !: number;
-  nrCheilita !: number;
-  nrFluoroza !: number;
-  nrFractura !: number;
-  nrHiperplazia !: number;
-  nrParodontoza !: number;
-  nrRetractie !: number;
-  nrSangerare !: number;
+  nrAbces = 0;
+  nrAmeloblastom = 0;
+  nrAngina = 0;
+  nrDiastema = 0;
+  nrGingivita = 0;
+  nrCarie = 0;
+  nrCheilita = 0;
+  nrFluoroza = 0;
+  nrFractura = 0;
+  nrHiperplazia = 0;
+  nrParodontoza = 0;
+  nrRetractie = 0;
+  nrSangerare = 0;
   constructor(private userService:UserService, private token:TokenStorageService, private istoricService: IstoricService) { }
 
   ngOnInit(): void {
-    // this.getChartLabels();
+    
     this.currentUser = this.token.getUser();
     this.userService.findUser(this.currentUser.email).subscribe({
       next:(response: User) => {
         this.loggedInUser = response;
         console.log(this.loggedInUser);
         this.getIstoric();
+        
       },
       error: (error: HttpErrorResponse) => {
         alert(error.message);
       }
-    });
+    })
   }
-
   chartType = 'bar';
 
   chartDatasets = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'My First dataset' }
+    { data: [this.nrAbces, this.nrAmeloblastom, this.nrAngina, this.nrDiastema, this.nrGingivita, this.nrCarie, this.nrCheilita, this.nrFluoroza, this.nrFractura, this.nrHiperplazia, this.nrParodontoza, this.nrRetractie, this.nrSangerare], label: 'Diagnostice' }
   ];
 
   chartLabels = ['Abces dentar', 'Ameloblastom', 'Angina Ludwig', 'Diastema', 'Gingivita', 'Caria dentara', 'Cheilita angulara', 'Fluoroza dentara', 'Fractura mandibulara', 'Hiperplazia gingivala', 'Parodontoza', 'Retractie gingivala', 'Sangerare gingivala'];
@@ -63,7 +63,14 @@ export class StatsComponent implements OnInit {
         'rgba(255, 206, 86, 0.2)',
         'rgba(75, 192, 192, 0.2)',
         'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(153, 102, 255, 0.2)'
       ],
       borderColor: [
         'rgba(255,99,132,1)',
@@ -71,7 +78,14 @@ export class StatsComponent implements OnInit {
         'rgba(255, 206, 86, 1)',
         'rgba(75, 192, 192, 1)',
         'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
+        'rgba(255, 159, 64, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(75, 192, 192, 1)'
       ],
       borderWidth: 2,
     }
@@ -94,16 +108,6 @@ export class StatsComponent implements OnInit {
     aux.push({data: this.array, label: 'Diagnostice'});
     return aux;
   }
-  // public getChartLabels(){
-
-  //   if(this.chartLabels.length === 0){
-  //      // @ts-ignore: Object is possibly 'null'.
-  //     for(let i=0; i<this.vehicles.length; i++){
-  //       // @ts-ignore: Object is possibly 'null'.
-  //       this.chartLabels.push(this.vehicles[i].denumire);
-  //     }
-  //   }
-  // } 
 
   public getIstoric(): void {
     this.istoricService.findIstoricById(this.loggedInUser.id).subscribe({
@@ -113,8 +117,8 @@ export class StatsComponent implements OnInit {
       }
     })
   }
-  public getData(): void{
 
+  public getData(): void{
     for(let istoric of this.istorics){
       if(istoric.diagnostic === "Abces dentar")
         this.nrAbces ++;
@@ -122,17 +126,43 @@ export class StatsComponent implements OnInit {
         this.nrAmeloblastom ++;
       if(istoric.diagnostic === "Angina Ludwig")
         this.nrAngina ++;
+      if(istoric.diagnostic === "Diastema")
+        this.nrDiastema ++;
+      if(istoric.diagnostic === "Gingivita")
+        this.nrGingivita ++;
+      if(istoric.diagnostic === "Caria dentara")
+        this.nrCarie ++;
+      if(istoric.diagnostic === "Cheilita angulara")
+        this.nrCheilita ++;
+      if(istoric.diagnostic === "Fluoroza dentara")
+        this.nrFluoroza ++;
+      if(istoric.diagnostic === "Fractura mandibulara")
+        this.nrFractura ++;
+      if(istoric.diagnostic === "Hiperplazia gingivala")
+        this.nrHiperplazia ++;
+      if(istoric.diagnostic === "Parodontoza")
+        this.nrParodontoza ++;
+      if(istoric.diagnostic === "Retractie gingivala")
+        this.nrRetractie ++;
+      if(istoric.diagnostic === "Sangerare gingivala")
+        this.nrSangerare ++;
     }
-    
-
     const  array2 = new Array<Number>();
-    // array2.push(this.pretCombustibil);
-    // array2.push(this.pretRCA);
-    // array2.push(this.pretCasco);
-    // array2.push(this.pretITP);
-    // array2.push(this.pretService);
-    // array2.push(this.pretAltceva);
+    array2.push(this.nrAbces);
+    array2.push(this.nrAmeloblastom);
+    array2.push(this.nrAngina);
+    array2.push(this.nrDiastema);
+    array2.push(this.nrGingivita);
+    array2.push(this.nrCarie);
+    array2.push(this.nrCheilita);
+    array2.push(this.nrFluoroza);
+    array2.push(this.nrFractura);
+    array2.push(this.nrHiperplazia);
+    array2.push(this.nrParodontoza);
+    array2.push(this.nrRetractie);
+    array2.push(this.nrSangerare);
 
     this.array = array2;
+
   }
 }
